d3a49273214516d9f02a72b5f49a91307a345364
pawello1001 <pawello1001@gmail.com>
2017-10-17 23:14:39 +0200

    sample google map implementation

--- app/src/main/java/com/example/pawe/organizer/flow/fragments/AddressesFragment.java
+++ app/src/main/java/com/example/pawe/organizer/flow/fragments/AddressesFragment.java
@@ -1,19 +1,36 @@
 package com.example.pawe.organizer.flow.fragments;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.pawe.organizer.MapsActivity;
 import com.example.pawe.organizer.R;
 
+import butterknife.ButterKnife;
+
 public class AddressesFragment extends Fragment {
+
+    Button siemaMapy;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_addresses, container, false);
+
+        ButterKnife.bind(rootView);
+        siemaMapy = ButterKnife.findById(rootView, R.id.siemaMapy);
+        siemaMapy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), MapsActivity.class));
+            }
+        });
+
         return rootView;
     }
 }
